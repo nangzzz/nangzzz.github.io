d0c2c6e4b6531833e28124533786da048b6fc418
xnangz <d10858803@gmail.com>
2022-07-22 10:57:28 +0700

    penambahan animasi

---
+++ assets/js/javascript.js
@@ -0,0 +1,6 @@
+var typed = new typed(".running", {
+    Strings : ["Jangan pernah berhenti belajar karena ilmu tidak akan pernah ada habisnya", "test1", "test3"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+})
