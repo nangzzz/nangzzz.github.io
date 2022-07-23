56215b252121d696ceafa490549d2b56c5b88001
xnangz <d10858803@gmail.com>
2022-07-23 13:44:16 +0700

    perbaikan UI

--- assets/js/javascript.js
+++
@@ -1,6 +0,0 @@
-var typed = new typed(".running", {
-    Strings : ["Jangan pernah berhenti belajar karena ilmu tidak akan pernah ada habisnya", "test1", "test3"],
-    typeSpeed: 150,
-    backSpeed: 150,
-    loop: true
-})
