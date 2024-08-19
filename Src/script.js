925d72fffcf571139dfd8494501a5e92fad3caf2
Danang Satria <106137675+nangzzz@users.noreply.github.com>
2024-08-20 00:38:21 +0700

    Minor Update

--- Src/script.js
+++ Src/script.js
@@ -165,7 +165,7 @@ function kataakhir(){
       },}).go();
 }
 function muncultombol3(){fungtom2=1;TombolWA.style="opacity:1;transform:scale(1)";}
-function menuju(){if(fungtom2==1){window.location = "https://api.whatsapp.com/send?phone=&text=" + pesanwhatsapp;}}
+function menuju(){if(fungtom2==1){window.location = "https://wa.me/6283838105024?text=" + pesanwhatsapp;}}
 
 tinggi = iniakhir.offsetHeight;
 function tentukantinggi(){tinggi = iniakhir.offsetHeight;}
